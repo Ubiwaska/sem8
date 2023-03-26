69ea3209678f5894dfc2c0e489916f9d0d3a9bd9
Полина Галкина <polygal@inbox.ru>
2023-03-26 13:29:11 +0300

    added 60

--- 54/Program.cs
+++ 54/Program.cs
@@ -1,65 +1,66 @@
 ﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// метод пузырька
 
-Console.Clear(); 
- 
-    Console.WriteLine("Enter the size of the array."); 
-    Console.WriteLine("Enter the number of rows:"); 
-    int number_rows=Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Enter the number of columns:"); 
-    int number_columns=Convert.ToInt32(Console.ReadLine());
-    
-    int[,] array = new int[number_rows, number_columns]; 
-    GetArray(array); 
-    PrintArray(array);
-    Console.WriteLine(); 
-    
-   SortEachRow(array);
-    PrintArray(array);  
-    
-   void SortEachRow(int[,] array)
+Console.Clear();
+
+Console.WriteLine("Enter the size of the array.");
+Console.WriteLine("Enter the number of rows:");
+int number_rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number of columns:");
+int number_columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[number_rows, number_columns];
+GetArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+SortEachRow(array);
+PrintArray(array);
+
+void SortEachRow(int[,] array)
+{
+    int n = array.GetLength(1);
+
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        int n=array.GetLength(1);
-
-        for (int i = 0; i < array.GetLength(0); i++) 
+        for (int j = 0; j < n; j++)
         {
-            for(int j=0; j<n; j++)
+            for (int k = 0; k < n - 1 - j; k++)
             {
-                for (int k=0; k<n-1-j; k++)  
-                {  
-                    if (array[i, k] < array[i, k + 1])
-                    {
+                if (array[i, k] < array[i, k + 1])
+                {
                     int temp = array[i, k + 1];
                     array[i, k + 1] = array[i, k];
                     array[i, k] = temp;
-                    }
                 }
-
             }
 
         }
-        
-    } 
-  
-     
-    void GetArray(int[,] array) 
-    { 
-        for (int i = 0; i < array.GetLength(0); i++) 
-        { 
-            for (int j = 0; j < array.GetLength(1); j++) 
-            { 
-                array[i, j] = new Random().Next(0, 10); 
-            } 
-        } 
-    }   
-     
-    void PrintArray(int[,] array) 
-    { 
-        for (int i = 0; i < array.GetLength(0); i++) 
-        { 
-            for (int j = 0; j < array.GetLength(1); j++) 
-            { 
-                Console.Write(array[i, j] + " "); 
-            } 
-            Console.WriteLine(); 
-        } 
+
     }
+
+}
+
+
+void GetArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
--- 60/Program.cs
+++ 60/Program.cs
@@ -1,2 +1,49 @@
-﻿
+﻿//Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Console.Clear();
 
+Console.WriteLine("Enter the size of array (n x m x p).");
+// p  (n x m)-matrices
+Console.WriteLine("Enter n:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter p:");
+int p = Convert.ToInt32(Console.ReadLine());
+
+int[,,] array = new int[n, m, p];
+GetArray(array);
+PrintArray(array);
+
+
+void GetArray(int[,,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(10, 99);
+            }
+        }
+    }
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int k = 0; k < array.GetLength(2); k++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(2); j++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
+
+}
